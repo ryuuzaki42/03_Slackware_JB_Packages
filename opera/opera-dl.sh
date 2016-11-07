c65e8e027fc1f0a58b794dba44ad9dc69029ba57
João Batista <joao42lbatista@gmail.com>
2016-11-07 01:44:23 -0200

    opera-dl.sh to generate a txz from a rpm

---
+++ opera/opera-dl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Create a txz from opera-stable-version.rpm
+
+progName="opera-stable"
+version="41.0.2353.46"
+linkDl="http://download4.operacdn.com/pub/opera/desktop/$version/linux"
+tag=JB-2
+
+
+if [ "$USER" != "root" ]; then
+    echo -e "\nNeed to be superuser (root)\nExiting\n"
+else
+    if [ -z "$ARCH" ]; then
+        case "$( uname -m )" in
+            i?86) ARCH=i386 ;;
+            arm*) ARCH=arm ;;
+            x86_64) ARCH=amd64 ;;
+            *) ARCH=$( uname -m ) ;;
+        esac
+    fi
+
+    if [ "$ARCH" == "amd64" ] ||[ "$ARCH" == "i386" ] ; then
+        wget -c "$linkDl/"$progName"_"$version"_$ARCH.rpm"
+    else
+        echo -e "\nError: $ARCH not configured\n"
+    fi
+
+    rpm2txz "$progName"_"$version"_$ARCH.rpm
+
+    rm "$progName"_"$version"_$ARCH.rpm
+
+    mv "$progName"_"$version"_$ARCH.txz "$progName"_"$version"_$ARCH-$tag.txz
+fi

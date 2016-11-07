5fe255c341591e1e09f240d4ce46bb2dc4dc846f
João Batista <joao42lbatista@gmail.com>
2016-11-07 10:38:43 -0200

    text change in the .sh

--- opera/opera-dl.sh
+++ opera/opera-dl.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
 # Create a txz from opera-stable-version.rpm
-
-progName="opera-stable"
-version="41.0.2353.46"
-linkDl="http://download4.operacdn.com/pub/opera/desktop/$version/linux"
-tag=JB-2
-
 
 if [ "$USER" != "root" ]; then
     echo -e "\nNeed to be superuser (root)\nExiting\n"
 else
+    progName="opera-stable"
+    version="41.0.2353.46"
+    linkDl="http://download4.operacdn.com/pub/opera/desktop/$version/linux"
+    tag=JB-2
+
     if [ -z "$ARCH" ]; then
         case "$( uname -m )" in
             i?86) ARCH=i386 ;;

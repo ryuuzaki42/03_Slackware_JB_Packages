47a6062f9f59bc377c84222a20868365e6584aeb
João Batista <joao42lbatista@gmail.com>
2016-12-12 15:09:03 -0200

    - to _

--- JB_scripts_dl/0_run_all.sh
+++ JB_scripts_dl/0_run_all.sh
@@ -3,20 +3,20 @@
 if [ "$USER" != "root" ]; then
     echo -e "\nNeed to be superuser (root)\nExiting\n"
 else
-    ./atom-dl-JBs.sh
+    ./atom_dl_JBs.sh
 
-    ./git-lfs-dl-JBs.sh
+    ./git_lfs_dl_JBs.sh
 
-    ./mendeleydesktop-dl-JBs.sh
+    ./mendeleydesktop_dl_JBs.sh
 
-    ./smartgit_and_smartsynchronize-dl-JBs.sh 1
+    ./smartgit_and_smartsynchronize_dl_JBs.sh 1
 
-    ./smartgit_and_smartsynchronize-dl-JBs.sh 2
+    ./smartgit_and_smartsynchronize_dl_JBs.sh 2
 
-    ./smplayer-dl-JBs.sh
+    ./smplayer_dl_JBs.sh
 
-    ./opera/opera-dl-JBs.sh
-    #./opera/cp_lib_opera-JBs.sh
+    ./opera/opera_dl_JBs.sh
+    #./opera/cp_lib_opera_JBs.sh
 
-    #./wps/wps-office-dl-JBs.sh
+    #./wps/wps_office_dl_JBs.sh
 fi

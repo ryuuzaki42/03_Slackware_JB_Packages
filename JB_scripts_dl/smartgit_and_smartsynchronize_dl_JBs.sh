1d818374a0f3c8eb6d1c1e355d84f6922dda35cf
João Batista <joao42lbatista@gmail.com>
2016-12-15 11:29:36 -0200

    smartsynchronize 3_4_9

--- JB_scripts_dl/smartgit_and_smartsynchronize_dl_JBs.sh
+++ JB_scripts_dl/smartgit_and_smartsynchronize_dl_JBs.sh
@@ -16,7 +16,7 @@ else
         progNameTmp="SmartGit"
         partFile="-linux"
     elif [ "$progBuild" == '2' ]; then
-        progName="smartsynchronize" # last tested: 3_4_8
+        progName="smartsynchronize" # last tested: 3_4_9
         progNameTmp="SmartSynchronize"
         partFile="-generic"
     else

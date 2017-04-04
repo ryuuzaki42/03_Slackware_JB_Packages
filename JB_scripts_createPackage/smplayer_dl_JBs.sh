403e0eb78031ec499e6ffa8a9a04c50a615389e4
João Batista <joao42lbatista@gmail.com>
2017-04-04 16:04:00 -0300

    smplayer "17.3.0" to "17.4.0"

--- JB_scripts_createPackage/smplayer_dl_JBs.sh
+++ JB_scripts_createPackage/smplayer_dl_JBs.sh
@@ -5,7 +5,7 @@
 if [ "$USER" != "root" ]; then
     echo -e "\nNeed to be superuser (root)\nExiting\n"
 else
-    progName="smplayer" # last tested: 17.3.0
+    progName="smplayer" # last tested: 17.4.0
     tag="JB"
 
     linkGetVersion="https://app.assembla.com/spaces/smplayer/subversion/source/HEAD/smplayer/trunk/OBS/Makefile?_format=raw"

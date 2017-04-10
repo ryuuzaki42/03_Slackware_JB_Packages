8e1d058481f8d20ddde9ec960b09bb0d3dc4701c
João Batista <joao42lbatista@gmail.com>
2017-04-09 22:39:53 -0300

    git_lfs_dl_JBs.sh - corrected some shell script errors

--- JB_scripts_createPackage/git_lfs_dl_JBs.sh
+++ JB_scripts_createPackage/git_lfs_dl_JBs.sh
@@ -9,21 +9,21 @@ else
     tag="JB"
 
     linkGetVersion="https://github.com/git-lfs/git-lfs/releases/latest"
-    wget $linkGetVersion -O $progName-latest
+    wget "$linkGetVersion" -O "${progName}-latest"
 
-    version=`cat $progName-latest | grep "/git-lfs/git-lfs/tree/v" | head -n 1 | cut -d "v" -f2 | cut -d "\"" -f1`
-    rm $progName-latest
+    version=$(cat $progName-latest | grep "/git-lfs/git-lfs/tree/v" | head -n 1 | cut -d "v" -f2 | cut -d "\"" -f1)
+    rm "${progName}-latest"
 
-    installedVersion=`ls /var/log/packages/$progName* | cut -d '-' -f3`
+    installedVersion=$(find /var/log/packages/$progName* | cut -d '-' -f3)
     echo -e "\n   Latest version: $version\nVersion installed: $installedVersion\n"
     if [ "$installedVersion" != '' ]; then
         if [ "$version" == "$installedVersion" ]; then
             echo -e "Version installed ($installedVersion) is equal to latest version ($version)"
             echo -n "Want continue? (y)es - (n)o (hit enter to no): "
 
-            continue=$1
+            continue="$1"
             if [ "$continue" == '' ]; then
-                read continue
+                read -r continue
             fi
 
             if [ "$continue" != 'y' ]; then
@@ -34,51 +34,43 @@ else
     fi
 
     if [ -z "$ARCH" ]; then
-    case "$( uname -m )" in
-        i?86) ARCH=i586 ;;
-        arm*) ARCH=arm ;;
-        *) ARCH=$( uname -m ) ;;
-    esac
+        case "$(uname -m)" in
+            i?86) ARCH="i586" ;;
+            arm*) ARCH="arm" ;;
+            *) ARCH=$(uname -m) ;;
+        esac
     fi
 
     if [ "$ARCH" = "i586" ]; then
-        SLKCFLAGS="-O2 -march=i586 -mtune=i686"
-        LIBDIRSUFFIX=""
-        SRCARCH=386
+        SRCARCH="386"
     elif [ "$ARCH" = "i686" ]; then
-        SLKCFLAGS="-O2 -march=i686 -mtune=i686"
-        LIBDIRSUFFIX=""
-        SRCARCH=386
+        SRCARCH="386"
     elif [ "$ARCH" = "x86_64" ]; then
-        SLKCFLAGS="-O2 -fPIC"
-        LIBDIRSUFFIX="64"
-        SRCARCH=amd64
+        SRCARCH="amd64"
     else
-        SLKCFLAGS="-O2"
-        LIBDIRSUFFIX=""
-        SRCARCH=386
+        SRCARCH="386"
     fi
 
-    folderDest=`pwd`
+    folderDest=$(pwd)
     linkDl="https://github.com/git-lfs/git-lfs/releases/download"
 
-    wget -c $linkDl/v$version/$progName-linux-$SRCARCH-$version.tar.gz
+    wget -c "$linkDl/v$version/${progName}-linux-${SRCARCH}-${version}.tar.gz"
 
-    tar xvf $progName-linux-$SRCARCH-$version.tar.gz
-    rm $progName-linux-$SRCARCH-$version.tar.gz
+    tar xvf "${progName}-linux-${SRCARCH}-${version}.tar.gz"
+    rm "${progName}-linux-${SRCARCH}-${version}.tar.gz"
 
-    cd $progName-$version
+    cd "${progName}-$version" || exit
 
     mkdir -p usr/bin
-    install -m0755 $progName usr/bin
+    install -m0755 "$progName" usr/bin
 
-    mkdir -p usr/doc/$progName-$version
-    mv *.md usr/doc/$progName-$version
+    mkdir -p "usr/doc/${progName}-$version"
+    mv ./*.md "usr/doc/${progName}-$version"
 
     rm git-lfs install.sh
 
-    /sbin/makepkg -l y -c n $folderDest/$progName-$version-$ARCH-$tag.txz
+    /sbin/makepkg -l y -c n "$folderDest/${progName}-${version}-${ARCH}-${tag}.txz"
 
-    cd ..
-    rm -r $progName-$version
+    cd .. || exit
+    rm -r "${progName}-$version"
 fi

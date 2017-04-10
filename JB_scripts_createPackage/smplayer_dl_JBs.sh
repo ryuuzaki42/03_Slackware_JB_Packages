9f20a0700490aae66c92689a9c0edcb1a2a9911e
João Batista <joao42lbatista@gmail.com>
2017-04-09 21:23:53 -0300

    smplayer_dl_JBs.sh - corrected some shell script errors

--- JB_scripts_createPackage/smplayer_dl_JBs.sh
+++ JB_scripts_createPackage/smplayer_dl_JBs.sh
@@ -9,12 +9,12 @@ else
     tag="JB"
 
     linkGetVersion="https://app.assembla.com/spaces/smplayer/subversion/source/HEAD/smplayer/trunk/OBS/Makefile?_format=raw"
-    wget $linkGetVersion -O $progName-latest
+    wget "$linkGetVersion" -O "${progName}-latest"
 
-    version=`cat $progName-latest | grep "VERSION" | head -n 1 | sed 's/[^0-9,.]*//g'`
-    rm $progName-latest
+    version=$(cat $progName-latest | grep "VERSION" | head -n 1 | sed 's/[^0-9,.]*//g')
+    rm "$progName-latest"
 
-    installedVersion=`ls /var/log/packages/$progName* | cut -d '-' -f2`
+    installedVersion=$(find /var/log/packages/$progName* | cut -d '-' -f2)
     echo -e "\n   Latest version: $version\nVersion installed: $installedVersion\n"
     if [ "$installedVersion" != '' ]; then
         if [ "$version" == "$installedVersion" ]; then
@@ -23,7 +23,7 @@ else
 
             continue=$1
             if [ "$continue" == '' ]; then
-                read continue
+                read -r continue
             fi
 
             if [ "$continue" != 'y' ]; then
@@ -42,56 +42,52 @@ else
     esac
     fi
 
-    initialFolder=`pwd`
+    initialFolder=$(pwd)
     progInstallFolder=$initialFolder/$progName-$version
     tmpFolder=$progInstallFolder-tmp
 
     if [ "$ARCH" = "i486" ]; then
         SLKCFLAGS="-O2 -march=i486 -mtune=i686"
-        LIBDIRSUFFIX=""
     elif [ "$ARCH" = "i686" ]; then
         SLKCFLAGS="-O2 -march=i686 -mtune=i686"
-        LIBDIRSUFFIX=""
     elif [ "$ARCH" = "x86_64" ]; then
         SLKCFLAGS="-O2 -fPIC"
-        LIBDIRSUFFIX="64"
     else
         SLKCFLAGS="-O2"
-        LIBDIRSUFFIX=""
     fi
 
     wget -c "$linkDl/$progName-$version.tar.bz2"
 
     set -e
 
-    mkdir $tmpFolder
-    cd $tmpFolder
+    mkdir "$tmpFolder"
+    cd "$tmpFolder" || exit
 
-    tar xvf $initialFolder/$progName-$version.tar.bz2
-    cd $progName-$version
+    tar xvf "$initialFolder/${progName}-${version}.tar.bz2"
+    cd "${progName}-$version" || exit
 
     chown -R root:root .
 
     sed -i "/^PREFIX/s/=.*$/=\/usr/;
-            /^DOC_PATH/s/\/.*$/\/doc\/$progName-$version/;
+            /^DOC_PATH/s/\/.*$/\/doc\/${progName}-$version/;
             s/share\/man/man/g;
             s/^QMAKE_OPTS=/QMAKE_OPTS+=/" Makefile
 
     QMAKE_OPTS="QMAKE_CXXFLAGS=\"$SLKCFLAGS\"" \
     make
-    make install DESTDIR=$progInstallFolder
+    make install DESTDIR="$progInstallFolder"
 
-    find $progInstallFolder -print0 | xargs -0 file | grep -e "executable" -e "shared object" | grep ELF \
+    find "$progInstallFolder" -print0 | xargs -0 file | grep -e "executable" -e "shared object" | grep ELF \
     | cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null || true
 
-    mkdir -p $progInstallFolder/usr/doc/$progName-$version
-    cp -a *.txt Changelog $progInstallFolder/usr/doc/$progName-$version
+    mkdir -p "$progInstallFolder/usr/doc/${progName}-$version"
+    cp -a ./*.txt Changelog "$progInstallFolder/usr/doc/${progName}-$version"
 
-    rm $progInstallFolder/usr/share/applications/smplayer_enqueue.desktop
+    rm "$progInstallFolder/usr/share/applications/smplayer_enqueue.desktop"
 
-    cd $progInstallFolder
-    /sbin/makepkg -l y -c n $initialFolder/$progName-$version-$ARCH-$tag.txz
+    cd "$progInstallFolder" || exit
+    /sbin/makepkg -l y -c n "$initialFolder/${progName}-${version}-${ARCH}-${tag}.txz"
 
-    cd $initialFolder
-    rm -r $tmpFolder $progName-$version $progName-$version.tar.bz2
+    cd "$initialFolder" || exit
+    rm -r "$tmpFolder" "${progName}-$version" "${progName}-${version}.tar.bz2"
 fi

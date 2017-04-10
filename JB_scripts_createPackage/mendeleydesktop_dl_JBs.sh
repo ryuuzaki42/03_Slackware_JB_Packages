76facd20503f6023dd6db411df0a7e01a9df9c86
João Batista <joao42lbatista@gmail.com>
2017-04-09 23:58:07 -0300

    mendeleydesktop_dl_JBs.sh - corrected some shell script errors

--- JB_scripts_createPackage/mendeleydesktop_dl_JBs.sh
+++ JB_scripts_createPackage/mendeleydesktop_dl_JBs.sh
@@ -9,21 +9,21 @@ else
     tag="JB"
 
     linkGetVersion="https://www.mendeley.com/release-notes/"
-    wget $linkGetVersion -O $progName-latest
+    wget "$linkGetVersion" -O "${progName}-latest"
 
-    version=`cat $progName-latest | grep "Release Notes for Mendeley Desktop" | head -n 1 | sed 's/[^0-9,.]*//g'`
-    rm $progName-latest
+    version=$(cat $progName-latest | grep "Release Notes for Mendeley Desktop" | head -n 1 | sed 's/[^0-9,.]*//g')
+    rm "${progName}-latest"
 
-    installedVersion=`ls /var/log/packages/$progName* | cut -d '-' -f2`
+    installedVersion=$(find /var/log/packages/$progName* | cut -d '-' -f2)
     echo -e "\n   Latest version: $version\nVersion installed: $installedVersion\n"
     if [ "$installedVersion" != '' ]; then
         if [ "$version" == "$installedVersion" ]; then
             echo -e "Version installed ($installedVersion) is equal to latest version ($version)"
             echo -n "Want continue? (y)es - (n)o (hit enter to no): "
 
-            continue=$1
+            continue="$1"
             if [ "$continue" == '' ]; then
-                read continue
+                read -r continue
             fi
 
             if [ "$continue" != 'y' ]; then
@@ -33,14 +33,14 @@ else
         fi
     fi
 
-    folderDest=`pwd`
+    folderDest=$(pwd)
     linkDl="http://desktop-download.mendeley.com/download/linux"
 
     if [ -z "$ARCH" ]; then
-        case "$( uname -m )" in
-            i?86) ARCH=i486 ;;
-            arm*) ARCH=arm ;;
-            *) ARCH=$( uname -m ) ;;
+        case "$(uname -m)" in
+            i?86) ARCH="i486" ;;
+            arm*) ARCH="arm" ;;
+            *) ARCH=$(uname -m) ;;
         esac
     fi
 
@@ -52,12 +52,11 @@ else
     elif [ "$ARCH" = "x86_64" ]; then
         LIBDIRSUFFIX="64"
     else
-        SLKCFLAGS="-O2"
         LIBDIRSUFFIX=""
     fi
 
     if [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "i486" ]; then
-        wget -c "$linkDl/$progName-$version-linux-$ARCH.tar.bz2"
+        wget -c "$linkDl/${progName}-${version}-linux-${ARCH}.tar.bz2"
     else
         echo -e "\nError: ARCH $ARCH not configured\n"
         exit 1
@@ -65,14 +64,14 @@ else
 
     set -e
 
-    folderSourceCode="$folderDest/$progName-$version-linux-$ARCH"
+    folderSourceCode="$folderDest/${progName}-${version}-linux-$ARCH"
 
-    rm -rf $folderSourceCode $folderSourceCode-tmp
+    rm -rf "$folderSourceCode" "$folderSourceCode-tmp"
 
-    tar xvf $folderDest/$progName-$version-linux-${ARCH}.tar.bz2
-    mv $progName-$version-linux-$ARCH $progName-$version-linux-$ARCH-tmp
+    tar xvf "$folderDest/${progName}-${version}-linux-${ARCH}.tar.bz2"
+    mv "${progName}-${version}-linux-$ARCH" "${progName}-${version}-linux-${ARCH}-tmp"
 
-    cd $progName-$version-linux-$ARCH-tmp
+    cd "${progName}-${version}-linux-${ARCH}-tmp" || exit
     chown -R root:root .
     find -L . \
     \( -perm 777 -o -perm 775 -o -perm 750 -o -perm 711 -o -perm 555 \
@@ -83,26 +82,26 @@ else
     # Remove the bundled qt since it should be present in the system already
     rm -rf lib/qt bin/qt.conf lib/mendeleydesktop/plugins
 
-    [ $ARCH = 'x86_64' ] && mv lib lib$LIBDIRSUFFIX
+    [ "$ARCH" = "x86_64" ] && mv lib "lib$LIBDIRSUFFIX"
 
     rm bin/mendeleydesktop
     ln -s ../lib${LIBDIRSUFFIX}/mendeleydesktop/libexec/mendeleydesktop.$ARCH bin/mendeleydesktop
 
     # Some docs lay on the top folder, so install them first
-    mkdir -p $folderSourceCode/usr/doc
-    mv share/doc/mendeleydesktop $folderSourceCode/usr/doc/$progName-$version
-    mv LICENSE README $folderSourceCode/usr/doc/$progName-$version
+    mkdir -p "$folderSourceCode/usr/doc"
+    mv share/doc/mendeleydesktop "$folderSourceCode/usr/doc/${progName}-$version"
+    mv LICENSE README "$folderSourceCode/usr/doc/${progName}-$version"
 
     rm INSTALL
     rmdir share/doc
 
-    mv * $folderSourceCode/usr
+    mv ./* "$folderSourceCode/usr"
 
-    find $folderSourceCode -print0 | xargs -0 file | grep -e "executable" -e "shared object" | grep ELF \
+    find "$folderSourceCode" -print0 | xargs -0 file | grep -e "executable" -e "shared object" | grep ELF \
     | cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null || true
 
-    cd $folderSourceCode
-    /sbin/makepkg -l y -c n $folderDest/$progName-$version-$ARCH-$tag.txz
+    cd "$folderSourceCode" || exit
+    /sbin/makepkg -l y -c n "$folderDest/${progName}-${version}-${ARCH}-${tag}.txz"
 
-    rm -rf $folderSourceCode $folderSourceCode-tmp $folderSourceCode.tar.bz2
+    rm -rf "$folderSourceCode" "${folderSourceCode}-tmp" "${folderSourceCode}.tar.bz2"
 fi

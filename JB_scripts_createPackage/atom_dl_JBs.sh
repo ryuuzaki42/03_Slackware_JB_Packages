620292c758808c29b20378301e9ac2933d3f0ed4
João Batista <joao42lbatista@gmail.com>
2017-04-09 23:13:41 -0300

    atom_dl_JBs.sh - corrected some shell script errors

--- JB_scripts_createPackage/atom_dl_JBs.sh
+++ JB_scripts_createPackage/atom_dl_JBs.sh
@@ -10,10 +10,10 @@ else
     linkGetVersion="https://github.com/atom/atom/releases/latest"
     wget $linkGetVersion -O $progName-latest
 
-    version=`cat $progName-latest | grep "Release.*[[:digit:]].*" | sed 's/[^0-9,.]*//g'`
+    version=$(cat $progName-latest | grep "Release.*[[:digit:]].*" | sed 's/[^0-9,.]*//g')
     rm $progName-latest
 
-    installedVersion=`ls /var/log/packages/$progName* | cut -d '-' -f2`
+    installedVersion=$(find /var/log/packages/$progName* | cut -d '-' -f2)
     echo -e "\n   Latest version: $version\nVersion installed: $installedVersion\n"
     if [ "$installedVersion" != '' ]; then
         if [ "$version" == "$installedVersion" ]; then
@@ -22,7 +22,7 @@ else
 
             continue=$1
             if [ "$continue" == '' ]; then
-                read continue
+                read -r continue
             fi
 
             if [ "$continue" != 'y' ]; then
@@ -31,26 +31,27 @@ else
             fi
         fi
     fi
+
     linkDl="https://github.com/atom/atom/releases/download"
 
     if [ -z "$ARCH" ]; then
-        case "$( uname -m )" in
-            i?86) ARCH=i386 ;;
-            arm*) ARCH=arm ;;
-            *) ARCH=$( uname -m ) ;;
+        case "$(uname -m)" in
+            i?86) ARCH="i386" ;;
+            arm*) ARCH="arm" ;;
+            *) ARCH=$(uname -m) ;;
         esac
     fi
 
-    if [ "$ARCH" == "x86_64" ]; then # Only 64 bits, without 32 bits rpm in atom site
-        wget -c $linkDl/v$version/$progName.$ARCH.rpm
+    if [ "$ARCH" == "x86_64" ]; then # Only 64 bits, without 32 bits precompiled rpm package in the atom repository/release
+        wget -c "$linkDl/v$version/${progName}.${ARCH}.rpm"
     else
         echo -e "\nError: ARCH $ARCH not configured\n"
         exit 1
     fi
 
-    mv $progName.$ARCH.rpm $progName-$version-$ARCH-$tag.rpm
+    mv "${progName}.${ARCH}.rpm" "${progName}-${version}-${ARCH}-${tag}.rpm"
 
-    rpm2txz $progName-$version-$ARCH-$tag.rpm
+    rpm2txz "${progName}-${version}-${ARCH}-${tag}.rpm"
 
-    rm $progName-$version-$ARCH-$tag.rpm
+    rm "${progName}-${version}-${ARCH}-${tag}.rpm"
 fi

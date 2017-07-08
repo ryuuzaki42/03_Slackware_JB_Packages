94878f77d7bd16f2809a941cb58b0586119037e3
João Batista <joao42lbatista@gmail.com>
2017-07-08 18:11:40 -0300

    Add header in the scripts and change date

--- JB_scripts_createPackage/smartgit_and_smartsynchronize_dl_JBs.sh
+++ JB_scripts_createPackage/smartgit_and_smartsynchronize_dl_JBs.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-# Create a txz from smartsynchronize and/or smartgit from "program"-version.tar.gz
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Create a txz from smartsynchronize and/or smartgit from "program"-version.tar.gz
 # Based in: https://slackbuilds.org/repository/14.2/development/smartgit/
+#
+# Last update: 08/07/2017
+#
+echo "\n# Create a txz from smartsynchronize and/or smartgit from \"program\"-version.tar.gz #\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\nNeed to be superuser (root)\nExiting\n"

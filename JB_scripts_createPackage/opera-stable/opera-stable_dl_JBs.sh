25bfe827d603c32d2924681cc70ac2ab39dabae8
João Batista <joao42lbatista@gmail.com>
2017-07-12 02:27:17 -0300

    opera-stable - update "46.0.2597.39" to "46.0.2597.46"

--- JB_scripts_createPackage/opera-stable/opera-stable_dl_JBs.sh
+++ JB_scripts_createPackage/opera-stable/opera-stable_dl_JBs.sh
@@ -29,7 +29,7 @@ echo -e "\n# Create a txz from opera-stable-version.rpm #\n"
 if [ "$USER" != "root" ]; then
     echo -e "\nNeed to be superuser (root)\nExiting\n"
 else
-    progName="opera-stable" # last tested: "46.0.2597.39"
+    progName="opera-stable" # last tested: "46.0.2597.46"
     tag="2_JB"
 
     linkGetVersion="http://ftp.opera.com/ftp/pub/opera/desktop/"

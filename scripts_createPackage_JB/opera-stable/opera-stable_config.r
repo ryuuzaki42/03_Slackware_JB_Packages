2efb7aea6968c216b5fd4ffd2f3b3d8ce5c91d08
João Batista <joao2008batista@hotmail.com>
2017-07-31 23:26:55 -0300

    opera-stable_config.r - update

--- scripts_createPackage_JB/opera-stable/opera-stable_config.r
+++ scripts_createPackage_JB/opera-stable/opera-stable_config.r
@@ -1,25 +1,18 @@
 ## Opera to Slackwre ##
 
 ## Run the script
-./opera_dl_JBs.sh
-
-    ## Or
-    # Download:
-    http://www.opera.com/download/get/?partner=www&opsys=Linux&package=RPM
-
-    # Create the txz package
-    rpm2txz opera-stable*.rpm
+./opera-stable_dl_JBs.sh
 
 ## Install
 installpkg opera-stable*.txz
 
 ## Add the MSE & H.264 plugins
-    Compile the "opera-ffmpeg-codecs" form Slackbuilds
-    https://slackbuilds.org/repository/14.2/multimedia/opera-ffmpeg-codecs/
+Compile the "opera-ffmpeg-codecs" form Slackbuilds
+https://slackbuilds.org/repository/14.2/multimedia/opera-ffmpeg-codecs/
 
-    # For 64 bits install compiled package opera-ffmpeg-codecs-*-x86_64-1_SBo.tgz
+## For 64 bits install the compiled package opera-ffmpeg-codecs-*-x86_64-1_JB.tgz
 
-    # For tests, go to:
-    https://www.youtube.com/html5
-    https://html5test.com
-    http://www.quirksmode.org/html5/tests/video.html
+## For tests, go to:
+https://www.youtube.com/html5
+https://html5test.com
+http://www.quirksmode.org/html5/tests/video.html

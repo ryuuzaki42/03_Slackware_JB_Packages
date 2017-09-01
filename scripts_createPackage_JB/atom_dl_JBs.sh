ac87ee95f96f3d033d15d84d8977797184d9575f
João Batista <joao2008batista@hotmail.com>
2017-09-01 09:06:52 -0300

    atom_dl_JBs.sh - update "1.19.4" to "1.19.5"

--- scripts_createPackage_JB/atom_dl_JBs.sh
+++ scripts_createPackage_JB/atom_dl_JBs.sh
@@ -22,14 +22,14 @@
 #
 # Script: Create a txz from atom-version.rpm
 #
-# Last update: 29/08/2017
+# Last update: 01/09/2017
 #
 echo -e "\n# Create a txz from atom-version.rpm #\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\nNeed to be superuser (root)\nExiting\n"
 else
-    progName="atom" # last tested: "1.19.4"
+    progName="atom" # last tested: "1.19.5"
     tag="1_JB"
 
     linkGetVersion="https://github.com/atom/atom/releases/latest"

a9da5f00b63883a5588dee23fedf7c7b43893f15
João Batista <joao2008batista@hotmail.com>
2017-11-16 09:58:23 -0200

    mendeleydesktop_dl_JBs.sh - Update "1.17.11" to "1.17.12"

--- scripts_createPackage_JB/mendeleydesktop_dl_JBs.sh
+++ scripts_createPackage_JB/mendeleydesktop_dl_JBs.sh
@@ -23,14 +23,14 @@
 # Script: Script to build a Slackware package of mendeleydesktop
 # Based in: https://slackbuilds.org/slackbuilds/14.2/academic/mendeleydesktop/
 #
-# Last update: 14/11/2017
+# Last update: 16/11/2017
 #
 echo -e "\\n# Script to build a Slackware package of mendeleydesktop #\\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="mendeleydesktop" # last tested: "1.17.11"
+    progName="mendeleydesktop" # last tested: "1.17.12"
     tag="1_JB"
 
     linkGetVersion="https://www.mendeley.com/release-notes/"

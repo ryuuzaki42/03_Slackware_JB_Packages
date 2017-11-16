d459f2fdfd0a348388a1cea6171bb9842aa9e72a
João Batista <joao2008batista@hotmail.com>
2017-11-16 09:59:04 -0200

    opera-stable_dl_JBs.sh - Update "49.0.2725.34" to "49.0.2725.39"

--- scripts_createPackage_JB/opera-stable/opera-stable_dl_JBs.sh
+++ scripts_createPackage_JB/opera-stable/opera-stable_dl_JBs.sh
@@ -22,14 +22,14 @@
 #
 # Script: Create a txz from opera-stable-version.rpm
 #
-# Last update: 14/11/2017
+# Last update: 16/11/2017
 #
 echo -e "\\n# Create a txz from opera-stable-version.rpm #\\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="opera-stable" # last tested: "49.0.2725.34"
+    progName="opera-stable" # last tested: "49.0.2725.39"
     tag="1_JB"
 
     linkGetVersion="http://ftp.opera.com/ftp/pub/opera/desktop/"

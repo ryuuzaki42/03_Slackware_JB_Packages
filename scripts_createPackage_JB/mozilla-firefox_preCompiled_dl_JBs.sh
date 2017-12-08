8568585b0d77a08a65d08be02a87f7e03a95e2eb
João Batista <joao2008batista@hotmail.com>
2017-12-08 15:04:50 -0200

    mozilla-firefox_preCompiled_dl_JBs.sh - update "57.0.1" to "57.0.2"

--- scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
@@ -22,10 +22,10 @@
 #
 # Script: Script to create a Slackware package from the mozilla-firefox stable pre-compiled
 #
-# Last update: 30/11/2017
+# Last update: 08/12/2017
 #
 echo -e "\\n# Script to create a Slackware package from the mozilla-firefox stable pre-compiled #\\n"
-progName="mozilla-firefox" # last tested: "57.0.1"
+progName="mozilla-firefox" # last tested: "57.0.2"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"

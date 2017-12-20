66504feac16168ea760d33b8785ff9185e2901ff
João Batista <joao42lbatista@gmail.com>
2017-12-20 12:50:12 -0200

    config_teamviewer.r rc.teamviewerd - Correction indentation

--- scripts_createPackage_JB/teamviewer/config_teamviewer.r
+++ scripts_createPackage_JB/teamviewer/config_teamviewer.r
@@ -4,20 +4,20 @@ fast and secure remote access to Linux, Windows PCs, and Macs.
 You must give execute permission on /etc/rc.d/rc.teamviewerd and run
 /etc/rc.d/rc.teamviewerd start prior launching TeamViewer application
 
-To make this process repeated on every boot sequence, add this line 
+To make this process repeated on every boot sequence, add this line
 in your /etc/rc.d/rc.local:
 
 if [ -x /etc/rc.d/rc.teamviewerd ]; then
     /etc/rc.d/rc.teamviewerd start
 fi
 
-NOTE:
-1. Newer version of Teamviewer can establish remote control connections
-to older version (version 3 and above), but not in the opposite direction.
-The same things goes to meetings (version 7 and above).
+## NOTE:
+    1. Newer version of Teamviewer can establish remote control connections
+    to older version (version 3 and above), but not in the opposite direction.
+    The same things goes to meetings (version 7 and above).
 
-2. Always stop teamviewerd service and remove the old version before
-attempting to upgrade as the path and configs may change on each major release.
+    2. Always stop teamviewerd service and remove the old version before
+    attempting to upgrade as the path and configs may change on each major release.
 
 ## Good commands
     teamviewer                  Start TeamViewer user interface (if not running)
@@ -29,9 +29,9 @@ attempting to upgrade as the path and configs may change on each major release.
     teamviewer daemon stop      Stop              TeamViewer daemon
     teamviewer daemon restart   Stop/Start        TeamViewer daemon
 
-## TeamViewer needs qt5 - Install these programs for a full qt5
-        qt5
-        qt5-webkit
-        libxkbcommon
-        libinput
-        libwacom
+## TeamViewer needs qt5 - Install these packages/programs for a full qt5
+    qt5
+    qt5-webkit
+    libxkbcommon
+    libinput
+    libwacom

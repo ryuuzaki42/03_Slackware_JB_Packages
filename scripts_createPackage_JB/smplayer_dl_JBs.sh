7f7e4ae35865b2fd6c20087dc548b839b9f57210
João Batista <joao42lbatista@gmail.com>
2017-12-21 21:42:12 -0200

    smplayer_dl_JBs.sh - update "17.11.2" to "17.12.0"

--- scripts_createPackage_JB/smplayer_dl_JBs.sh
+++ scripts_createPackage_JB/smplayer_dl_JBs.sh
@@ -23,14 +23,14 @@
 # Script: Script to build a Slackware package of smplayer
 # Based in: https://slackbuilds.org/repository/14.2/multimedia/smplayer/
 #
-# Last update: 17/12/2017
+# Last update: 21/12/2017
 #
 echo -e "\\n# Script to build a Slackware package of smplayer (without skins and themes) #\\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="smplayer" # last tested: "17.11.2"
+    progName="smplayer" # last tested: "17.12.0"
     tag="1_JB"
 
     linkGetVersion="https://app.assembla.com/spaces/smplayer/subversion/source/HEAD/smplayer/trunk/OBS/Makefile?_format=raw"

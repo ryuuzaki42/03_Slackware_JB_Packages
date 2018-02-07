78a3bde3f84dd071244fdc3a532f97749ec78103
João Batista <joao42lbatista@gmail.com>
2018-02-07 21:38:27 -0200

    mozilla-firefox_preCompiled_dl_JBs.sh - update "58.0.1" to "58.0.2"

--- scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the mozilla-firefox stable pre-compiled
 #
-# Last update: 30/01/2018
+# Last update: 07/02/2018
 #
 echo -e "\\n# Script to create a Slackware package from the mozilla-firefox stable pre-compiled #\\n"
 
@@ -40,7 +40,7 @@ else
             ;;
     esac
 
-    progName="mozilla-firefox" # last tested: "58.0.1"
+    progName="mozilla-firefox" # last tested: "58.0.2"
     tag="1_JB"
     folderDest=$(pwd)
 

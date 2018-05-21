4cea1047d0d41b16f313239ca7fe2b6e18a34ce8
João Batista <joao42lbatista@gmail.com>
2018-05-21 14:49:30 -0300

    shellcheck_preCompiled_dl_JBs.sh - up "0.4.7_git5b226e7" to "0.4.7_git77546fb"

--- scripts_createPackage_JB/shellcheck_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/shellcheck_preCompiled_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the shellcheck pre-compiled
 #
-# Last update: 14/05/2018
+# Last update: 21/05/2018
 #
 echo -e "\\n# Script to create a Slackware package from the shellcheck pre-compiled #\\n"
 
@@ -38,7 +38,7 @@ else
         exit 1
     fi
 
-    progName="shellcheck" # last tested: "0.4.7_git5b226e7"
+    progName="shellcheck" # last tested: "0.4.7_git77546fb"
     tag="1_JB"
     folderDest=$(pwd)
 

401928137fa55390a4f3838cc347f27462917e55
João Batista <joao42lbatista@gmail.com>
2018-05-31 14:46:51 -0300

    atom_dl_JBs.sh - up "1.27.1" to "1.27.2"

--- scripts_createPackage_JB/atom_dl_JBs.sh
+++ scripts_createPackage_JB/atom_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Create a txz from atom-version.rpm
 #
-# Last update: 21/05/2018
+# Last update: 31/05/2018
 #
 echo -e "\\n# Create a txz from atom-version.rpm #\\n"
 
@@ -42,7 +42,7 @@ else
         exit 1
     fi
 
-    progName="atom" # last tested: "1.27.1"
+    progName="atom" # last tested: "1.27.2"
     tag="1_JB"
 
     linkGetVersion="https://github.com/atom/atom/releases/latest"

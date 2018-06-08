5f25b499efc111c0b5fe3a0c110f310011b581de
João Batista <joao42lbatista@gmail.com>
2018-06-08 13:04:47 -0300

    teamviewer_dl_JBs.sh - up "13.1.3026" to "13.1.8286"

--- scripts_createPackage_JB/teamviewer/teamviewer_dl_JBs.sh
+++ scripts_createPackage_JB/teamviewer/teamviewer_dl_JBs.sh
@@ -23,14 +23,14 @@
 # Script: Script to build a Slackware package of teamviewer
 # Based in: http://slackbuilds.org/repository/14.2/network/teamviewer/
 #
-# Last update: 21/03/2018
+# Last update: 08/06/2018
 #
 echo "This script create a txz version from teamviewer_arch.deb"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="teamviewer" # last tested: "13.1.3026"
+    progName="teamviewer" # last tested: "13.1.8286"
     tag="1_JB"
 
     folderDest=$(pwd)

0d8b4167a1ec5bb3d13d21050b8b84c52e463c48
João Batista <joao42lbatista@gmail.com>
2018-11-09 17:25:57 -0200

    opera_dl_JBs.sh - "56.0.3051.52" to "56.0.3051.99"

--- scripts_createPackage_JB/opera/opera_dl_JBs.sh
+++ scripts_createPackage_JB/opera/opera_dl_JBs.sh
@@ -22,14 +22,14 @@
 #
 # Script: Create a txz from opera-stable-version.rpm
 #
-# Last update: 17/10/2018
+# Last update: 09/11/2018
 #
 echo -e "\\n# Create a txz from opera-stable-version.rpm #\\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="opera" # last tested: "56.0.3051.52"
+    progName="opera" # last tested: "56.0.3051.99"
     tag="1_JB"
 
     linkGetVersion="http://ftp.opera.com/ftp/pub/opera/desktop/"

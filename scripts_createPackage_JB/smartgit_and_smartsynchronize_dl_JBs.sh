9e0bb95cdf76d00b8f7f2f2c78da615712500f56
João Batista <joao42lbatista@gmail.com>
2019-01-30 11:30:02 -0200

    smartgit_and_smartsynchronize_dl_JBs.sh - "smartgit" - up "18_2_4" to "18_2_5"

--- scripts_createPackage_JB/smartgit_and_smartsynchronize_dl_JBs.sh
+++ scripts_createPackage_JB/smartgit_and_smartsynchronize_dl_JBs.sh
@@ -23,7 +23,7 @@
 # Script: Create a txz from smartsynchronize and/or smartgit from "program"-version.tar.gz
 # Based in: https://slackbuilds.org/repository/14.2/development/smartgit/
 #
-# Last update: 17/01/2019
+# Last update: 30/01/2019
 #
 echo -e "\\n# Create a txz from smartsynchronize and/or smartgit from \"program\"-version.tar.gz #\\n"
 
@@ -37,7 +37,7 @@ else
     fi
 
     if [ "$progBuild" == '1' ]; then
-        progName="smartgit" # last tested: "18_2_4"
+        progName="smartgit" # last tested: "18_2_5"
         countF='2'
     elif [ "$progBuild" == '2' ]; then
         progName="smartsynchronize" # last tested: "3_5_1"

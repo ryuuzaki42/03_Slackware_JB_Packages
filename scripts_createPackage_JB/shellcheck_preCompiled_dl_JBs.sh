1f3301dcc4ee5673fc58ca265cc7deff8b719f6c
João Batista <joao42lbatista@gmail.com>
2019-05-09 07:33:56 -0300

    shellcheck_preCompiled_dl_JBs.sh - up "0.6.0_gite2e65e1" to "0.6.0_git2521c1c"

--- scripts_createPackage_JB/shellcheck_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/shellcheck_preCompiled_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the shellcheck pre-compiled
 #
-# Last update: 03/05/2019
+# Last update: 09/05/2019
 #
 echo -e "\\n# Script to create a Slackware package from the shellcheck pre-compiled #\\n"
 
@@ -35,7 +35,7 @@ else
         exit 1
     fi
 
-    progName="shellcheck" # last tested: "0.6.0_gite2e65e1"
+    progName="shellcheck" # last tested: "0.6.0_git2521c1c"
     tag="1_JB"
     folderDest=$(pwd)
 

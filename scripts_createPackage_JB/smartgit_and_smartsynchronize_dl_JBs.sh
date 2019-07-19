c8713a7e4492d4c1ea3104762a639777aac9c4f0
João Batista <joao42lbatista@gmail.com>
2019-07-18 23:05:56 -0300

    smartgit_and_smartsynchronize_dl_JBs.sh - "smartsynchronize" - up "3_5_1" to "4_0_0"

--- scripts_createPackage_JB/smartgit_and_smartsynchronize_dl_JBs.sh
+++ scripts_createPackage_JB/smartgit_and_smartsynchronize_dl_JBs.sh
@@ -23,7 +23,7 @@
 # Script: Create a txz from smartsynchronize and/or smartgit from "program"-version.tar.gz
 # Based in: https://slackbuilds.org/repository/14.2/development/smartgit/
 #
-# Last update: 19/06/2019
+# Last update: 18/07/2019
 #
 echo -e "\\n# Create a txz from smartsynchronize and/or smartgit from \"program\"-version.tar.gz #\\n"
 
@@ -40,7 +40,7 @@ else
         progName="smartgit" # last tested: "18_2_9"
         countF='2'
     elif [ "$progBuild" == '2' ]; then
-        progName="smartsynchronize" # last tested: "3_5_1"
+        progName="smartsynchronize" # last tested: "4_0_0"
         countF='1'
     else
         echo -e "\\nError: The chosen program ($progBuild) is unknown\\n"
@@ -83,11 +83,17 @@ else
     folderDest=$(pwd)
     tag="1_JB"
 
-    wget -c "$linkDl/${progName}-linux-${version}.tar.gz"
+    if [ "$progBuild" == '1' ]; then
+        versionTmp=$version
+    elif [ "$progBuild" == '2' ]; then
+        versionTmp=${version//_/.}
+    fi
+
+    wget -c "$linkDl/${progName}-linux-${versionTmp}.tar.gz"
 
     rm -r "$progName" 2> /dev/null
 
-    tar -xvf "${progName}-linux-${version}.tar.gz"
+    tar -xvf "${progName}-linux-${versionTmp}.tar.gz"
 
     cd "$progName" || exit
     mkdir -p "usr/doc/${progName}-$version"
@@ -166,5 +172,5 @@ $progName:" >> install/slack-desc
 
     cd .. || exit
     rm -r "$progName"
-    rm "${progName}-linux-${version}.tar.gz"
+    rm "${progName}-linux-${versionTmp}.tar.gz"
 fi

53efd970bfab6bd5528d8d34f24b939f36411abf
João Batista <joao42lbatista@gmail.com>
2019-09-27 13:58:53 -0300

    skypeforlinux_dl_JBs.sh - up "8.52.76.134" to "8.53.76.53"

--- scripts_createPackage_JB/others/skypeforlinux_dl_JBs.sh
+++ scripts_createPackage_JB/others/skypeforlinux_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the SkypeForLinux pre-compiled
 #
-# Last update: 16/09/2019
+# Last update: 27/09/2019
 #
 echo -e "\\n# Script to create a Slackware package from the SkypeForLinux pre-compiled #\\n"
 
@@ -42,7 +42,7 @@ fi
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="skypeforlinux" # last tested: "8.52.76.134"
+    progName="skypeforlinux" # last tested: "8.53.76.53"
     linkProg="https://repo.skype.com/deb/pool/main/s/skypeforlinux"
     linkSlackbuilds142Prog="https://slackbuilds.org/slackbuilds/14.2/network/skypeforlinux.tar.gz"
 

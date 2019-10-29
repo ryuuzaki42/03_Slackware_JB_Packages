089ea0d407119eb09da863dbdd55b83f3d9b205c
João Batista <joao42lbatista@gmail.com>
2019-10-29 09:54:01 -0300

    opera_dl_JBs.sh - up "69.0.3" to "70.0.0"

--- scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the mozilla-firefox stable pre-compiled
 #
-# Last update: 10/10/2019
+# Last update: 29/10/2019
 #
 echo -e "\\n# Script to create a Slackware package from the mozilla-firefox stable pre-compiled #\\n"
 
@@ -40,7 +40,7 @@ else
             ;;
     esac
 
-    progName="mozilla-firefox" # last tested: "69.0.3"
+    progName="mozilla-firefox" # last tested: "70.0.0"
     tag="1_JB"
     folderDest=$(pwd)
 

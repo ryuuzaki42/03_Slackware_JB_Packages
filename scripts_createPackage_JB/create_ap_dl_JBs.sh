b20608ac2388567fd52734907f9b36807b55ce60
João Batista <joao42lbatista@gmail.com>
2020-02-25 21:37:25 -0300

    create_ap_dl_JBs.sh - up "0.4.6_gitd67a5a5" to "0.4.6_git048ca4e"

--- scripts_createPackage_JB/create_ap_dl_JBs.sh
+++ scripts_createPackage_JB/create_ap_dl_JBs.sh
@@ -24,14 +24,14 @@
 #
 # Link: https://github.com/oblique/create_ap
 #
-# Last update: 12/11/2019
+# Last update: 25/02/2020
 #
 echo -e "\\n# Script to build a Slackware package of create_ap #\\n"
 
 if [ "$USER" != "root" ]; then
     echo -e "\\nNeed to be superuser (root)\\nExiting\\n"
 else
-    progName="create_ap" # last tested: "0.4.6_gitd67a5a5"
+    progName="create_ap" # last tested: "0.4.6_git048ca4e"
     arch="noArch"
     tag="1_JB"
 

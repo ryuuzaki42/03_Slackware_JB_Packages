24d480b3273faa125822297003ccb20196d3258c
João Batista <joao42lbatista@gmail.com>
2020-08-18 10:03:03 -0300

    smartgit_and_smartsynchronize_dl_JBs.sh - "smartsynchronize" - up "4_1_1_1" to "4_1_1"

--- scripts_createPackage_JB/others/smartgit_and_smartsynchronize_dl_JBs.sh
+++ scripts_createPackage_JB/others/smartgit_and_smartsynchronize_dl_JBs.sh
@@ -23,7 +23,7 @@
 # Script: Create a txz from smartsynchronize and/or smartgit from "program"-version.tar.gz
 # Based in: https://slackbuilds.org/repository/14.2/development/smartgit/
 #
-# Last update: 12/08/2020
+# Last update: 18/08/2020
 #
 echo -e "\\n# Create a txz from smartsynchronize and/or smartgit from \"program\"-version.tar.gz #\\n"
 
@@ -40,7 +40,7 @@ else
         progName="smartgit" # last tested: "20_1_4"
         countF='2'
     elif [ "$progBuild" == '2' ]; then
-        progName="smartsynchronize" # last tested: "4_1_1_1"
+        progName="smartsynchronize" # last tested: "4_1_1"
         countF='1'
     else
         echo -e "\\nError: The chosen program ($progBuild) is unknown\\n"

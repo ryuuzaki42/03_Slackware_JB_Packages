55d9e164743cdbbd4795bba25ad09dc9d3e30e9c
João Batista <joao42lbatista@gmail.com>
2020-09-08 19:51:48 -0300

    config_opera.r - add spaces, new html5 link test and scrit now has stable in the name

--- scripts_createPackage_JB/opera/config_opera.r
+++ scripts_createPackage_JB/opera/config_opera.r
@@ -1,17 +1,22 @@
 ## Opera to Slackwre ##
 
 ## Run the script = Only for 64 bits
-    ./opera_dl_JBs.sh
+    ./opera-stable_dl_JBs.sh
 
 ## Install opera and opera-ffmpeg-codecs
     installpkg opera-*.txz
 
 ## Or compile from slackbuilds
     http://slackbuilds.org/repository/14.2/network/opera/
+
     https://slackbuilds.org/repository/14.2/multimedia/opera-ffmpeg-codecs/
 
 ## The package opera-ffmpeg-codecs Add the MSE & H.264 plugins
     ## For tests, go to:
         https://www.youtube.com/html5
+
         https://html5test.com
+
+        https://html5test.opensuse.org
+
         http://www.quirksmode.org/html5/tests/video.html

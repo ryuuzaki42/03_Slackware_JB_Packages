375867d1f21293d5db8bb266f9105c9b85dc079f
João Batista <joao42lbatista@gmail.com>
2021-09-07 22:28:17 -0300

    mozilla-firefox_preCompiled_dl_JBs.sh - up "91.0.2" to "92.0.0"

--- scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the mozilla-firefox stable pre-compiled
 #
-# Last update: 27/08/2021
+# Last update: 07/09/2021
 #
 set -e
 
@@ -42,7 +42,7 @@ else
             ;;
     esac
 
-    progName="mozilla-firefox" # last tested: "91.0.2"
+    progName="mozilla-firefox" # last tested: "92.0.0"
     tag="1_JB"
     folderDest=$(pwd)
 

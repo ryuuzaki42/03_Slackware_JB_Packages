77fa747959dd91cabae9f9d0c5d8f7cdf2424ac7
João Batista <joao42lbatista@gmail.com>
2022-06-08 11:14:34 -0300

    opera config.r - up to Slackware 15.0

--- scripts_createPackage_JB/opera/opera config.r
+++ scripts_createPackage_JB/opera/opera config.r
@@ -3,20 +3,24 @@
 ## Run the script = Only for 64 bits
     ./opera-stable_dl_JBs.sh
 
-## Install opera and opera-ffmpeg-codecs
-    installpkg opera-*.txz
+## Install opera
+    installpkg opera-stable-*.txz
 
-## Or compile from slackbuilds
-    http://slackbuilds.org/repository/14.2/network/opera/
+    ## Or compile from slackbuilds
+        https://slackbuilds.org/repository/15.0/network/opera/
 
-    https://slackbuilds.org/repository/14.2/multimedia/opera-ffmpeg-codecs/
+## Install opera-ffmpeg-codecs
+    installpkg opera-ffmpeg-codecs-*.txz
 
-## The package opera-ffmpeg-codecs Add the MSE & H.264 plugins
-    ## For tests, go to:
-        https://www.youtube.com/html5
+    ## Or compile from slackbuilds
+    https://slackbuilds.org/repository/15.0/multimedia/opera-ffmpeg-codecs/
 
-        https://html5test.com
+    ## The package opera-ffmpeg-codecs add the MSE & H.264 plugins
+        ## For tests, go to:
+            https://www.youtube.com/html5
 
-        https://html5test.opensuse.org
+            https://html5test.com
 
-        http://www.quirksmode.org/html5/tests/video.html
+            https://html5test.opensuse.org
+
+            http://www.quirksmode.org/html5/tests/video.html

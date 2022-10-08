bfca4d9afbd1ee3799b2ad529374ea651620cd9d
João Batista <joao42lbatista@gmail.com>
2022-10-08 16:53:27 -0300

    atom_dl_JBs.sh - up "1.58.0" to "1.60.0"

--- scripts_createPackage_JB/others/atom_dl_JBs.sh
+++ scripts_createPackage_JB/others/atom_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Create a txz from atom-version.rpm
 #
-# Last update: 24/10/2021
+# Last update: 08/10/2022
 #
 echo -e "\\n# Create a txz from atom-version.rpm #\\n"
 
@@ -42,7 +42,7 @@ else
         exit 1
     fi
 
-    progName="atom" # last tested: "1.58.0"
+    progName="atom" # last tested: "1.60.0"
     tag="1_JB"
 
     linkGetVersion="https://github.com/atom/atom/releases/latest"

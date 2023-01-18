af4c81b4855c358d49b99d4156866bdb438b6618
João Batista <joao42lbatista@gmail.com>
2023-01-17 21:33:37 -0300

    mozilla-firefox_preCompiled_dl_JBs.sh - up  "108.0.2" to "109.0"

--- scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
+++ scripts_createPackage_JB/mozilla-firefox_preCompiled_dl_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to create a Slackware package from the mozilla-firefox stable pre-compiled
 #
-# Last update: 05/01/2023
+# Last update: 17/01/2023
 #
 echo -e "\\n# Script to create a Slackware package from the mozilla-firefox stable pre-compiled #\\n"
 
@@ -40,7 +40,7 @@ else
             ;;
     esac
 
-    progName="mozilla-firefox" # last tested: "108.0.2"
+    progName="mozilla-firefox" # last tested: "109.0"
     tag="1_JB"
     folderDest=$(pwd)
 

5403410653ee32eb72f94da57c89068058e6f69d
João Batista <joao42lbatista@gmail.com>
2023-04-19 07:49:29 -0300

    check_for_update_progs_JBs.sh - up "112.0" to "112.0.1"

--- scripts_createPackage_JB/check_for_update_progs_JBs.sh
+++ scripts_createPackage_JB/check_for_update_progs_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to check if some programs has one update
 #
-# Last update: 15/04/2023
+# Last update: 18/04/2023
 #
 # Tip: Pass "win" as parameter to call the windowsPrograms
 # Tip: Pass "all" as parameter to call program updates
@@ -156,7 +156,7 @@ mkvtoolnix (){
 }
 
 mozilla-firefox(){
-    progName="mozilla-firefox" # last tested: "112.0"
+    progName="mozilla-firefox" # last tested: "112.0.1"
     link="https://www.mozilla.org/en-US/firefox/all"
     command="grep 'latest-firefox' a.html | sed 's/.*latest-firefox=\"//; s/\" .*//'"
 

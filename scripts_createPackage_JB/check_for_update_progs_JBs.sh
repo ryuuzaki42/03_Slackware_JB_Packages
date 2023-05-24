0bef964e227382d2b6fc2a9ab35dd0dce4be296a
João Batista <joao42lbatista@gmail.com>
2023-05-24 10:13:14 -0300

    check_for_update_progs_JBs.sh - up 24/05/2023

--- scripts_createPackage_JB/check_for_update_progs_JBs.sh
+++ scripts_createPackage_JB/check_for_update_progs_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Script to check if some programs has one update
 #
-# Last update: 18/05/2023
+# Last update: 24/05/2023
 #
 # Tip: Pass "win" as parameter to call the windowsPrograms
 # Tip: Pass "all" as parameter to call program updates
@@ -158,7 +158,7 @@ mkvtoolnix (){
 }
 
 mozilla-firefox(){
-    progName="mozilla-firefox" # last tested: "113.0.1"
+    progName="mozilla-firefox" # last tested: "113.0.2"
     link="https://www.mozilla.org/en-US/firefox/all"
     command="grep 'latest-firefox' a.html | sed 's/.*latest-firefox=\"//; s/\" .*//'"
 
@@ -226,7 +226,7 @@ smplayer(){
 teamviewer(){
     progName="teamviewer" # last tested: "15.41.7"
     link="https://www.teamviewer.com/en/download/linux"
-    command="grep -o 'deb package .*' a.html | head -n 1 | tr -d 'a-z <>/'"
+    command="grep 'Current version' a.html | tr -d 'Ca-z: <>/'"
 
     checkVersion "$progName" "$link" "$command"
 }

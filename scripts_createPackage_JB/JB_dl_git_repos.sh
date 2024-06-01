4265a5abcff91501fec265fd439ee37445b10612
João Batista <joao42lbatista@gmail.com>
2024-06-01 16:36:33 -0300

    Update JB_dl_git_repos.sh - correct links names

--- scripts_createPackage_JB/JB_dl_git_repos.sh
+++ scripts_createPackage_JB/JB_dl_git_repos.sh
@@ -3,7 +3,7 @@
 # Clone some of my repositories
 # https://github.com/ryuuzaki42
 #
-# Last update: 31/05/2024
+# Last update: 01/06/2024
 #
 set -x
 
@@ -12,14 +12,14 @@ cd /media/sda2/git_clone/
 
 git clone https://ryuuzaki42@github.com/ryuuzaki42/01_Cool_Things.git
 
-git clone https://ryuuzaki42@github.com/ryuuzaki42/02_Scripts_Slackware.git
+git clone https://ryuuzaki42@github.com/ryuuzaki42/02_Scripts_Linux.git
 cd 02_Scripts_Slackware/ || exit
 ./0install_scripts_JBi.sh
 cd ../
 
 git clone https://ryuuzaki42@github.com/ryuuzaki42/03_Slackware_JB_Packages.git
 
-git clone https://ryuuzaki42@github.com/ryuuzaki42/04_AppImage_shortcut_desktop.git
+git clone https://ryuuzaki42@github.com/ryuuzaki42/04_AppImage_Shortcut_Desktop.git
 
 git clone https://ryuuzaki42@github.com/ryuuzaki42/05_Estrutura_Dados.git
 
@@ -28,12 +28,12 @@ git clone https://ryuuzaki42@github.com/ryuuzaki42/05_Estrutura_Dados.git
 
 #git clone https://ryuuzaki42@github.com/ryuuzaki42/13_clone_multilib_Slackware_repo.git
 
-#git clone https://ryuuzaki42@github.com/ryuuzaki42/14_Nvidia_Driver_Slackware_Laptop/
+#git clone https://ryuuzaki42@github.com/ryuuzaki42/14_Nvidia_Driver_Slackware/
 
 ## Rename repo
-git clone https://ryuuzaki42@github.com/ryuuzaki42/26_dl_PVANet_files.git
+git clone https://ryuuzaki42@github.com/ryuuzaki42/26_DL_PVANet_files.git
 
 ## Old
-#git clone https://ryuuzaki42@github.com/ryuuzaki42/25_security_Wi-Fi
+#git clone https://ryuuzaki42@github.com/ryuuzaki42/25_Security_Wi-Fi
 
 #git clone https://ryuuzaki42@github.com/99_Old_Files.git

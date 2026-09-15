902ea0b2244357299b944055393578ca64e5e2c3
João Batista <joao42lbatista@gmail.com>
2026-09-15 19:33:30 -0300

    Update virtualbox_dl_JBs.sh

--- slackware_dls/virtualbox_dl_JBs.sh
+++ slackware_dls/virtualbox_dl_JBs.sh
@@ -21,7 +21,7 @@
 #
 # Descrição: Script to download the last version VirtualBox
 #
-# Last update: 18/08/2026
+# Last update: 15/09/2026
 #
 case "$(uname -m)" in
     i?86) archDL="x86" ;;
@@ -29,7 +29,7 @@ case "$(uname -m)" in
     *) archDL=$(uname -m) ;;
 esac
 
-progName="virtualbox" # last tested: "7.2.16"
+progName="virtualbox" # last tested: "7.2.18"
 
 linkGetVersion="https://www.virtualbox.org/wiki/Downloads"
 wget "$linkGetVersion" -O "${progName}_latest"

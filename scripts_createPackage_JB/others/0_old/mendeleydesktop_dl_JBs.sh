c949686dccca99eb29d7eed43113b27184d7a5a2
João Batista <joao42lbatista@gmail.com>
2026-09-22 13:26:07 -0300

    atom_dl_JBs.sh, create_ap_dl_JBs.sh, mendeleydesktop_dl_JBs.sh, and 1 more file - update license2

--- scripts_createPackage_JB/others/0_old/mendeleydesktop_dl_JBs.sh
+++ scripts_createPackage_JB/others/0_old/mendeleydesktop_dl_JBs.sh
@@ -12,8 +12,8 @@
 #
 # Este programa é distribuído na esperança de que seja útil,
 # mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
-# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO. Consulte a
-# Licença Pública Geral do GNU para mais detalhes.
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO.
+# Consulte a Licença Pública Geral do GNU para mais detalhes.
 #
 # Script: Script to build a Slackware package of mendeleydesktop
 # Based in: https://slackbuilds.org/slackbuilds/14.2/academic/mendeleydesktop/

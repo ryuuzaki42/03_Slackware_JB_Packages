073cb8851773bc25b16849a905a1bd2cee514f1d
João Batista <joao42lbatista@gmail.com>
2026-09-22 13:27:02 -0300

    0_run_all.sh, mozilla-firefox_preCompiled_dl_JBs.sh, git-lfs_dl_JBs.sh, and 2 more files - update license2

--- scripts_createPackage_JB/others/smartgit_and_smartsynchronize_dl_JBs.sh
+++ scripts_createPackage_JB/others/smartgit_and_smartsynchronize_dl_JBs.sh
@@ -12,8 +12,8 @@
 #
 # Este programa é distribuído na esperança de que seja útil,
 # mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
-# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO. Consulte a
-# Licença Pública Geral do GNU para mais detalhes.
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO.
+# Consulte a Licença Pública Geral do GNU para mais detalhes.
 #
 # Script: Create a txz from smartsynchronize and/or smartgit from "program"-version.tar.gz
 # Based in: https://slackbuilds.org/repository/14.2/development/smartgit/

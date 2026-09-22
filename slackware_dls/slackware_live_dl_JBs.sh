576b55d311663e06f25b9bc4ae0da6424d3cacf3
João Batista <joao42lbatista@gmail.com>
2026-09-22 11:17:17 -0300

    Update slackware_live_dl_JBs.sh

--- slackware_dls/slackware_live_dl_JBs.sh
+++ slackware_dls/slackware_live_dl_JBs.sh
@@ -15,6 +15,8 @@
 # COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO.
 # Consulte a Licença Pública Geral do GNU para mais detalhes.
 #
+# Descrição: Script to download the last version of Slackware Live, made by AlienBob
+#
 # Last update: 14/02/2025
 #
 # My downloads:

87d63df05ae4c8772940cad5f8f157fa682768ea
João Batista <joao42lbatista@gmail.com>
2026-09-22 13:27:58 -0300

    updateScripts_JBs.sh - update license2

--- scripts_createPackage_JB/updateScripts_JBs.sh
+++ scripts_createPackage_JB/updateScripts_JBs.sh
@@ -12,8 +12,8 @@
 #
 # Este programa é distribuído na esperança de que seja útil,
 # mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
-# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO. Consulte a
-# Licença Pública Geral do GNU para mais detalhes.
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO.
+# Consulte a Licença Pública Geral do GNU para mais detalhes.
 #
 # Script: Scripts to update the scripts with "last tested" and "Last update" from Slackware packages (txz) updates
 #

1ac0105cffbe823a668c2334dc05a777c964865b
João Batista <joao42lbatista@gmail.com>
2026-09-22 11:00:11 -0300

    check_for_update_progs_JBs.sh, libreoffice_dl_alien_JBs.sh, slackware_live_dl_JBs.sh, and 1 more file - update license

--- slackware_dls/slackware_live_dl_JBs.sh
+++ slackware_dls/slackware_live_dl_JBs.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
+#
 # Autor= João Batista Ribeiro
 # Bugs, Agradecimentos, Críticas "construtivas"
 # me envie um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
-# Este programa é um software livre; você pode redistribui-lo e/ou
-# modifica-lo dentro dos termos da Licença Pública Geral GNU como
-# publicada pela Fundação do Software Livre (FSF); na versão 2 da
-# Licença, ou (na sua opinião) qualquer versão.
+# Este programa é um software livre: você pode redistribuí-lo e/ou
+# modificá-lo sob os termos da Licença Pública Geral GNU (GPL)
+# conforme publicada pela Free Software Foundation, tanto a versão 3
+# da licença, como (a seu critério) qualquer versão posterior.
 #
-# Este programa é distribuído na esperança que possa ser útil,
-# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
-# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
-#
-# Veja a Licença Pública Geral GNU para mais detalhes.
-# Você deve ter recebido uma cópia da Licença Pública Geral GNU
-# junto com este programa, se não, escreva para a Fundação do Software
-#
-# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-#
-# Descrição: Script to download the last version of Slackware Live, made by AlienBob
+# Este programa é distribuído na esperança de que seja útil,
+# mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO.
+# Consulte a Licença Pública Geral do GNU para mais detalhes.
 #
 # Last update: 14/02/2025
 #
@@ -28,6 +22,7 @@
 #     Stable  - only one option - ./slackware_live_dl_JBs.sh 1 . 2 1 y
 #     Current - only one option - ./slackware_live_dl_JBs.sh 1 . 3 1 y
 #
+
 echo -e "\nScript to download the last version of Slackware Live (made by Alien Bob)\n"
 
 # Last tested:
